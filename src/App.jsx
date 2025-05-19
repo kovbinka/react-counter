0dfe960ff58a21b6ce02a12790df3beed183c946
kovbinka <roman10kovbinka@gmail.com>
2025-05-19 23:03:55 +0200

    img fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import './App.css'
 import { incrementCounterHandler } from './handlers/increment'
 import { decrementCounterHandler } from './handlers/decrement'
 import { resetCounterHandler } from './handlers/reset'
+import CounterLogo from './assets/797-counter.svg'
 
 function App() {
   const [counter, setCounter] = useState(0);
 
   return (
     <div>
-      <img className='logo' src="/src/assets/797-counter.svg" alt="counter" />
+      <img className='logo' src={CounterLogo} alt="counter" />
       <h2>Counter: {counter}</h2>
       <button onClick={() => incrementCounterHandler(setCounter)}>Increment➕</button>
       <button onClick={() => resetCounterHandler(setCounter)}>Reset0️⃣</button>
